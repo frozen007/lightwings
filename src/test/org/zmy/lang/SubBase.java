a17676f4dfcad0d5cbef7b31f34892f393d1ef1a
frozen007 <frozen007@sohu.com>
2012-02-29 21:35:36 +0800

    sync

---
+++ src/test/org/zmy/lang/SubBase.java
@@ -0,0 +1,13 @@
+package test.org.zmy.lang;
+
+public class SubBase extends Base {
+
+//    public SubBase() {
+//        System.out.println("SubBase()");
+//    }
+//
+    public SubBase(int x) {
+        System.out.println("SubBase(int x)");
+    }
+    
+}
